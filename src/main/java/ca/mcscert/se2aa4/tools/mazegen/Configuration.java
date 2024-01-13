77e30aeb638f29573f3dc46ab7a9d06eac525f91
bilalm04 <bilal71612@gmail.com>
2024-01-12 22:18:31 -0500

    added default values for maze configuration

--- src/main/java/ca/mcscert/se2aa4/tools/mazegen/Configuration.java
+++ src/main/java/ca/mcscert/se2aa4/tools/mazegen/Configuration.java
@@ -1,5 +1,18 @@
 package ca.mcscert.se2aa4.tools.mazegen;
 
-public class Configuration {
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
+public class Configuration {
+    public Configuration(String[] args) { }
+
+    public long seed() { return System.currentTimeMillis(); }
+
+    public int width() { return 41; }
+
+    public int height() { return 41; }
+
+    public BufferedWriter outputFile() {
+        return new BufferedWriter(new OutputStreamWriter(System.out));
+    }
 }
